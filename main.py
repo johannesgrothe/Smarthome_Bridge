90c88d6a6b76aa9f16745a723074291f91816a2d
Johannes Grothe <johannesgrothe@web.de>
2020-09-18 20:27:54 +0200

    Worked on protocol and added method to connect to client

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ parser.add_argument('--mqtt_pw', help='mqtt password to be uploaded.')
 
 parser.add_argument('--network_only', action='store_true', help='needs a config file. uploads only the network config.')
 parser.add_argument('--id_only', action='store_true', help='needs a config file. uploads only the id.')
+parser.add_argument('--monitor_mode', action='store_true', help='only uses the script as a read-only serial monitor')
 ARGS = parser.parse_args()
 
 
@@ -45,26 +46,77 @@ def decode_line(line):
     return False
 
 
-def read_serial(timeout=5):
+def send_serial(path, body):
+    if isinstance(body, dict):
+        str_body = json.dumps(body)
+    elif isinstance(body, str):
+        str_body = body
+    else:
+        print("Body has illegal type")
+        return False
+    if not isinstance(path, str):
+        print("Path has illegal type")
+        return False
+    req_line = "!r_p[{}]_b[{}]_\n".format(path, str_body)
+    print("Sending '{}'".format(req_line[:-1]))
+    ser.write(req_line.encode())
+
+
+def read_serial(timeout=0, monitor_mode=False):
     print("Reading from serial port...")
     timeout_time = time.time() + timeout
     while True:
         try:
-            ser_bytes = ser.readline().decode("utf-8")
-            buf_req = decode_line(ser_bytes)
-            if buf_req:
-                return buf_req
+            ser_bytes = ser.readline().decode()
+            # print("   -> {}".format(ser_bytes[:-1]))
+            if monitor_mode:
+                print(ser_bytes[:-1])
+            else:
+                if ser_bytes.startswith("Backtrace: 0x"):
+                    print("Client crashed with {}".format(ser_bytes[:-1]))
+                    return False
+                buf_req = decode_line(ser_bytes)
+                if buf_req:
+                    return buf_req
         except (FileNotFoundError, serial.serialutil.SerialException):
             print("Lost connection to serial port")
             return False
-        if time.time() > timeout_time:
+        if (timeout > 0) and (time.time() > timeout_time):
             print("Connection timed out")
             return False
 
 
+def connect_to_client():
+    client_id = None
+    session_id = None
+    client_ack = False
+    print("Put your client in 'Serial Only'-Mode and click restart.")
+    while client_id is None:
+        in_req = read_serial(10)
+        if in_req and in_req["p"] == "debug/register":
+            buf_body = in_req["b"]
+            if "chip_id" in buf_body and "session_id" in buf_body:
+                client_id = buf_body["chip_id"]
+                session_id = buf_body["session_id"]
+
+    buf_req = {}
+    buf_req["pc_id"] = "blubmacbook"
+    buf_req["session_id"] = session_id
+    send_serial("debug/register", buf_req)
+
+    while not client_ack:
+        in_req = read_serial(10)
+        if in_req and in_req["p"] == "debug/register":
+            buf_body = in_req["b"]
+            if "ack" in buf_body and buf_body["ack"]:
+                client_ack = True
+
+    print("Client '{}' connected.".format(client_id))
+
+
 if __name__ == '__main__':
 
-    print(decode_line("!r_p[yolokopter]_b[{}]_"))
+    # print(decode_line("!r_p[yolokopter]_b[{}]_"))
 
     if ARGS.port:
         serial_port = ARGS.port
@@ -85,6 +137,11 @@ if __name__ == '__main__':
         print("Unable to connect to serial port '{}'".format(serial_port))
 
     if serial_active:
-        read_buffer = read_serial()
-        if (read_buffer):
-            pprint(read_buffer)
+        if ARGS.monitor_mode:
+            read_serial(0, True)
+        else:
+            connect_to_client()
+
+    #     read_buffer = read_serial()
+    #     if (read_buffer):
+    #         pprint(read_buffer)
--- protocol.md
+++ protocol.md
@@ -2,3 +2,11 @@
 
 ### Frame Structure
 `!r_p[<path: string>]_b[<body: json>]_\r\n`
+
+### Communication Secification
+
+| Sender         | Direction    | Path               | Payload
+| :------------- | :----------: | -----------:       | -----------:
+| Chip           | PC           | `debug/register`   | `{"chip_id": "<chip_id>", "session_id": <session_id>}`
+| PC             | Chip         | `debug/register`   | `{"pc_id": "<pc_id>", "session_id": <session_id>}`
+| Chip           | PC           | `debug/register`   | `{"ack": true, "session_id": <session_id>}`
