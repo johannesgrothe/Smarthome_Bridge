64b47036bb8cabe772e3ee67d6f772eed9ffd225
Johannes Grothe <johannesgrothe@web.de>
2020-12-21 14:37:36 +0100

    Added selecting upload port

--- chip_flasher.py
+++ chip_flasher.py
@@ -1,14 +1,21 @@
 import os
 import argparse
+from typing import Optional
 
 parser = argparse.ArgumentParser(description='Script to flash different software versions to the chip')
 parser.add_argument('--branch', help='git branch to flash on the chip')
+parser.add_argument('--serial_port', help='serial port for uploading')
 ARGS = parser.parse_args()
 
 
-def flash_chip(branch_name: str, force_reset: bool = False) -> bool:
+def flash_chip(branch_name: str, force_reset: bool = False, upload_port: Optional[str] = None) -> bool:
     repo_name = "Smarthome_ESP32"
     repo_url = "git@github.com:A20GameCo/{}.git".format(repo_name)
+
+    upload_port_phrase = ""
+    if upload_port is not None:
+        print("Manually setting upload port to '{}'".format(upload_port))
+        upload_port_phrase = " --upload-port {}".format(upload_port)
 
     repo_works = False
 
@@ -40,7 +47,7 @@ def flash_chip(branch_name: str, force_reset: bool = False) -> bool:
 
     print("OK.\n".format(branch_name))
 
-    uploading_successful = os.system("cd {};pio run --target upload".format(repo_name)) == 0
+    uploading_successful = os.system("cd {};pio run --target upload{}".format(repo_name, upload_port_phrase)) == 0
 
     print("\n")
 
@@ -57,5 +64,9 @@ if __name__ == '__main__':
     branch = "develop"
     if ARGS.branch:
         branch = ARGS.branch
-    flash_chip(branch)
+    if ARGS.serial_port:
+        flash_chip(branch,
+                   upload_port=ARGS.serial_port)
+    else:
+        flash_chip(branch)
     print("Done")
