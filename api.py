219d04d74dce841839c32469b64d071516cf9116
Johannes Grothe <johannesgrothe@web.de>
2020-12-27 15:33:53 +0100

    Unified datetime format

--- api.py
+++ api.py
@@ -58,7 +58,7 @@ def run_api(bridge, port: int):
         buf_res = {"bridge_name": bridge_name,
                    "software_commit": bridge.get_sw_commit(),
                    "software_branch": bridge.get_sw_branch(),
-                   "running_since": str(bridge.get_time_launched()),
+                   "running_since": bridge.get_time_launched().strftime("%Y-%m-%d %H:%M:%S"),
                    "gadget_count": len(gadget_list),
                    "connector_count": len(connector_list),
                    "client_count": len(client_list)}
--- smarthomeclient.py
+++ smarthomeclient.py
@@ -1,10 +1,10 @@
 """Module for the SmarthomeClient Class"""
-from time import time
 from datetime import datetime
 from typing import Optional
 
 # Maximum timeout in seconds before the client is considered inactive
 max_timeout = 17
+
 
 def filter_mapping(in_map: dict) -> (bool, dict):
     """Filters a port mapping dict to not contain any non-int or negative keys and no double values.
@@ -40,10 +40,10 @@ class SmarthomeClient:
     __name: str
 
     # When the client was last connected
-    __last_connected: int
+    __last_connected: datetime
 
     # When the client was created
-    __created: int
+    __created: datetime
 
     # A unique runtime id provided by the chip. Has to change on every reboot of the client
     __runtime_id: int
@@ -52,7 +52,7 @@ class SmarthomeClient:
     __needs_update: bool
 
     # Date the chip was flashed (if available)
-    __flash_date: Optional[datetime]
+    __flash_time: Optional[datetime]
 
     # Software-commit hash
     __software_commit: Optional[str]
@@ -68,16 +68,17 @@ class SmarthomeClient:
 
     def __init__(self, name: str, runtime_id: int):
         self.__name = name
-        self.__last_connected = 0
-        self.__created = int(time())
+        self.__last_connected = datetime(1900, 1, 1)
+        self.__created = datetime.now()
         self.__runtime_id = runtime_id
         self.__needs_update = True
 
-        self.__flash_date = None
+        self.__flash_time = None
         self.__software_commit = None
         self.__software_branch = None
 
-        self.__boot_mode = 0
+        # Set boot mode to "Unknown_Mode"
+        self.__boot_mode = 3
 
         has_err, self.__port_mapping = filter_mapping({})
 
@@ -85,21 +86,21 @@ class SmarthomeClient:
         """Returns the name of the client"""
         return self.__name
 
-    def get_created(self) -> int:
+    def get_created(self) -> datetime:
         """Gets the timestamp when the client was created in seconds since the epoch"""
         return self.__created
 
-    def get_last_connected(self) -> int:
+    def get_last_connected(self) -> datetime:
         """Returns when the client was last active in seconds since the epoch"""
         return self.__last_connected
 
     def trigger_activity(self):
         """Reports any activity of the client"""
-        self.__last_connected = int(time())
+        self.__last_connected = datetime.now()
 
     def is_active(self) -> bool:
         """Returns whether the client is still considered active"""
-        return self.__last_connected + max_timeout > int(time())
+        return self.__last_connected.timestamp() + max_timeout > datetime.now().timestamp()
 
     def update_runtime_id(self, runtime_id: bool):
         """Updates the current runtime_id, sets internal 'needs_update'-flag if it changed"""
@@ -115,7 +116,7 @@ class SmarthomeClient:
                     software_branch: Optional[str], port_mapping: dict, boot_mode: int):
         """Reports an successful update to the client"""
 
-        self.__flash_date = datetime.strptime(flash_date, "%Y-%m-%d")
+        self.__flash_time = datetime.strptime(flash_date, "%Y-%m-%d %H:%M:%S")
         self.__software_commit = software_commit
         self.__software_branch = software_branch
         self.__boot_mode = boot_mode
@@ -129,12 +130,12 @@ class SmarthomeClient:
     def serialized(self) -> dict:
         """Returns a serialized version of the client"""
         out_date = None
-        if self.__flash_date is not None:
-            out_date = str(self.__flash_date.date())
+        if self.__flash_time is not None:
+            out_date = self.__flash_time.strftime("%Y-%m-%d %H:%M:%S")
 
         return {"name": self.__name,
-                "created": self.__created,
-                "last_connected": self.__last_connected,
+                "created": self.__created.strftime("%Y-%m-%d %H:%M:%S"),
+                "last_connected": self.__last_connected.strftime("%Y-%m-%d %H:%M:%S"),
                 "is_active": self.is_active(),
                 "boot_mode": self.__boot_mode,
                 "sw_uploaded": out_date,
@@ -146,9 +147,9 @@ class SmarthomeClient:
         """Returns the port mapping of the client"""
         return self.__port_mapping
 
-    def get_sw_flash_date(self) -> Optional[datetime]:
+    def get_sw_flash_time(self) -> Optional[datetime]:
         """Returns the date, the software was written on the chip"""
-        return self.__flash_date
+        return self.__flash_time
 
     def get_sw_commit(self) -> Optional[str]:
         """Returns the software commit if available"""
