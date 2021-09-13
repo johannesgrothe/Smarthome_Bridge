696b6b44b4494f28c9ffd331b0286a8856370963
Johannes Grothe <johannesgrothe@web.de>
2021-09-13 14:45:43 +0200

    #29 Fixed mqtt credentials container test

--- tests/network/test_mqtt_credentials_container.py
+++ tests/network/test_mqtt_credentials_container.py
@@ -8,22 +8,12 @@ def test_mqtt_credentials_container():
         MqttCredentialsContainer(None, 1883)
 
     with pytest.raises(MqttCredentialsError):
-        MqttCredentialsContainer("yolokopter", 1883)
-
-    with pytest.raises(MqttCredentialsError):
-        MqttCredentialsContainer("192.168.178", 1883)
-
-    with pytest.raises(MqttCredentialsError):
-        MqttCredentialsContainer("192.168.-5.44", 1883)
-
-    with pytest.raises(MqttCredentialsError):
-        MqttCredentialsContainer("192.168.111.256", 1883)
-
-    with pytest.raises(MqttCredentialsError):
         MqttCredentialsContainer("192.168.111.256", -5)
 
     with pytest.raises(MqttCredentialsError):
         MqttCredentialsContainer("192.168.111.256", "yolo")
+
+    assert MqttCredentialsContainer("mosquitto", 1883).has_auth() is False
 
     assert MqttCredentialsContainer("192.168.178.111", 1883).has_auth() is False
 
